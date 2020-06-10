5c060de6f19be823efbae81656c0bfc9ca6ecc29
liyang <7325560@qq.com>
2020-06-10 08:56:44 +0800

    adjust install script to link config into HOME directory.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/wood9366/dot-alacritty.git ~/.dot-alacritty
-ln -s ~/.dot-alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml 
+ln -s ~/.dot-alacritty/alacritty.yml ~/.alacritty.yml
